d5259982d93f77267a3c5a5c4dfb38cc2658d35e
Marcus <marc303i@stud.kea.dk>
2022-09-13 18:55:31 +0200

    Har tilført lidt noter

--- Postnummer/src/Controller.java
+++ Postnummer/src/Controller.java
@@ -4,21 +4,29 @@ import java.util.Scanner;
 
 public class Controller {
 
+    // Programmet sættes igang
     public static void run(){
+        // Bygger en ArrayListe af PostInfo objekter ud fra metoden creatPostInfoList()
         ArrayList<PostInfo> postInfos = createPostInfoList();
+        // Beder brugeren om at indtaste et nummer
         System.out.print("Indtast venligst postnummer: ");
+        // Gemmer brugerens svar via metoden getUserInputString()
         String brugerSvar = getUserInputString();
 
+        // Laver en boolean der holder øje emd om postnummeret blev fundet
         boolean isPostNumFound = false;
 
+        // For hver postInfo i postInfoListen spørger vi om brugerens svar og postInfoNummeret er ens
         for (int i = 0; i < postInfos.size(); i++) {
             PostInfo postInfoObject = postInfos.get(i);
             String postInfoNumber = postInfoObject.getPostNummer();
             if (postInfoNumber.equals(brugerSvar)) {
+                // Hvis vi finder et match til brugerens svar, så sætter vi isPostNumFound til true og printer pladsnummer, samt postInfoObjekt
                 isPostNumFound = true;
                 System.out.println("Nr. " + (i+1)  + ":\t" + postInfoObject);
             }
         }
+        // Hvis brugerens svar ikke er fundet printes der brugerens svar ikke er valid
         if (!isPostNumFound) {
             System.out.println(brugerSvar + " is not a valid answer!");
         }
